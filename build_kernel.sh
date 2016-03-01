5a2b3cf475816795856743e5315184919b1a00d5
Sumit <androidlover5842@gmail.com>
2016-03-01 14:56:34 +0530

    MKBOOTIMAGE KYLEVE
    added bootimage creator for kyleve (GT-S7392)

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,107 @@
 #!/bin/bash
+echo "Made By KadjarT Edited by SR"
+echo "#################"
+echo "Choose the device"
+echo "#################"
+ choice=""
+while [ "$choice" != "q" ]
+do
+echo "  kyleve  "
+echo "  kylevess   " &&   echo -n ""
+read choice
 
-mkdir -p kernel_out
+case $choice in
+
+kyleve)
+echo "#################"
+echo "$choice selected"
+echo "#################"
+export PATH=/home/sr/cm12.2/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-eabi-
+mkdir -p kernel_out 
 cp -f ./arch/arm/configs/bcm21664_hawaii_ss_kyleve_rev00_defconfig ./kernel_out/.config
 make -j8 ARCH=arm KBUILD_OUTPUT=./kernel_out oldnoconfig
 make -j8 ARCH=arm KBUILD_OUTPUT=./kernel_out
 cp -f ./kernel_out/arch/arm/boot/zImage ./arch/arm/boot/
 cp -rf ./kernel_out/arch/arm/boot/compressed/vmlinux ./arch/arm/boot/compressed/
 cp -rf ./kernel_out/vmlinux ./
+mkdir tmp
+cd tmp
+cp -f ../arch/arm/boot/zImage ./zImage
+cp ../arch/arm/tools/boot.img-ramdisk.cpio.gz-kyleve ./boot.img-ramdisk.cpio.gz
+cp ../arch/arm/tools/mkbootimg ./mkbootimg
+cp ../arch/arm/tools/META-INF_kyleve.zip ./META-INF.zip
+echo "Creating boot.img"
+./mkbootimg --kernel zImage --ramdisk boot.img-ramdisk.cpio.gz --board kyleve --pagesize 4096 --base 0x82000000 -o boot.img
+echo "boot.img has been created"
+unzip META-INF.zip
+echo "#################"
+echo "Creating flashable zip"
+echo "#################"
+zip -r tmp META-INF boot.img
+echo "#################"
+echo "flashable zip has been created for $choice"
+echo "#################"
+mv -f tmp.zip ../flash_kyleve.zip
+mv -f boot.img ../boot.img
+echo "#################"
+echo "flashble zip is ready you can under"
+cd ..
+rm -rf tmp
+pwd
+echo "#################"
+cd ../
+exit
+
+;;
+
+kylevess)
+echo "#################"
+echo " $choice selected"
+echo "#################"
+export PATH=/home/sr/cm12.1/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-eabi-
+mkdir -p kernel_out
+cp -f ./arch/arm/configs/bcm21664_hawaii_ss_kylevess_rev00_defconfig ./kernel_out/.config
+make -j8 ARCH=arm KBUILD_OUTPUT=./kernel_out oldnoconfig
+make -j8 ARCH=arm KBUILD_OUTPUT=./kernel_out
+cp -f ./kernel_out/arch/arm/boot/zImage ./arch/arm/boot/
+cp -rf ./kernel_out/arch/arm/boot/compressed/vmlinux ./arch/arm/boot/compressed/
+cp -rf ./kernel_out/vmlinux ./
+mkdir tmp
+cd tmp
+cp -f ../arch/arm/boot/zImage ./zImage
+cp ../arch/arm/tools/kylevess-ramdisk.gz ./boot.img-ramdisk.cpio.gz
+cp ../arch/arm/tools/mkbootimg ./mkbootimg
+cp ../arch/arm/tools/META-INF_kylevess.zip ./META-INF.zip
+echo "#################"
+echo "Creating boot.img"
+echo "#################"
+./mkbootimg --kernel zImage --ramdisk boot.img-ramdisk.cpio.gz --board kylevess --pagesize 4096 --base 0x82000000 -o boot.img
+echo "#################"
+echo "boot.img has been created"
+echo "#################"
+unzip META-INF.zip
+echo "#################"
+echo "Creating flashable zip"
+echo "#################"
+zip -r tmp META-INF boot.img
+echo "#################"
+echo "flashable zip has been created for $choice."
+echo "#################"
+mv -f tmp.zip ../flash_kylevess.zip
+mv -f boot.img ../boot.img
+echo "#################"
+echo "flashble zip is ready you can under"
+cd ..
+rm -rf tmp
+pwd
+echo "#################"
+cd ../
+exit
+esac
+done
