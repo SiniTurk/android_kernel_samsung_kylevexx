02ef68d9f56fc6f5f28889869afa41f3f993552c
grace5921 <androidlover5842@gmail.com>
2016-06-07 01:02:26 +0530

    build: Security and net: fix compilation error

--- build_kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p kernel_out
-cp -f ./arch/arm/configs/bcm21664_hawaii_ss_kylevess_rev00_defconfig ./kernel_out/.config
-make -j8 ARCH=arm KBUILD_OUTPUT=./kernel_out oldnoconfig
-make -j8 ARCH=arm KBUILD_OUTPUT=./kernel_out
-cp -f ./kernel_out/arch/arm/boot/zImage ./arch/arm/boot/
-cp -rf ./kernel_out/arch/arm/boot/compressed/vmlinux ./arch/arm/boot/compressed/
-cp -rf ./kernel_out/vmlinux ./
